d88a2d1ce870d0a5440619cc9ad3a7f9c006fe7e
huy <huy246302@gmail.com>
2024-07-30 16:02:07 +0700

    missingAtributteHomePage

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -30,7 +30,7 @@ export default async function ProtectedPage() {
       </div>
 
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
+        <Header isHomePage={false} />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           <FetchDataSteps />
